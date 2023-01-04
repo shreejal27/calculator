1de662d551cab32e4ca20cd624a51cb1e84731df
Shreejal Maharjan <shreejal27@gmail.com>
2023-01-04 19:01:25 +0545

    fixed lower calculator screen

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 const numberButtons = document.querySelectorAll(".operand");
-const operatorButtons = document.querySelectorAll("operator");
-const equalsButton = document.querySelector("equals");
-const deleteButton = document.querySelector("delete");
-const clearButton = document.querySelector("clear");
-const upperClassTextElement = document.querySelector("upperScreen");
-const lowerClassTextElement = document.querySelector("lowerScreen");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+const deleteButton = document.querySelector(".delete");
+const clearButton = document.querySelector(".clear");
+const upperClassTextElement = document.getElementById("top");
+const lowerClassTextElement = document.getElementById("bottom");
 
-var currentOperand= 0;
-var previousOperand= 0;
+let values = [];
 
 numberButtons.forEach(button => {
     button.addEventListener('click', ()=>{
         appendNumber(button.innerText);
-        updateDisplay(button.innerText);
+  
     });
-})
+});
 
-function handleClick(){
-    console.log("Clickced");
+operatorButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        check(button.innerText);
+    });
+});
+
+equalsButton.addEventListener('click', ()=>{
+        check("=");
+    });
+
+
+deleteButton.addEventListener('click', ()=>{
+        values.pop();
+        updateLowerDisplay();
+    });
+
+clearButton.addEventListener('click', ()=>{
+        clear();
+    });
+
+
+function check(clickItem){
+    console.log(clickItem);
 }
+
 
 function clear(){
-
+    upperClassTextElement.innerText="";
+    lowerClassTextElement.innerText="0";
 }
 
-function deletee(){
+function del(){
 
 }
 
 function appendNumber(number){
-  
+    values.push(number);
+    updateLowerDisplay();
  
 }
 
@@ -41,6 +64,7 @@ function calculate(){
 
 }
 
-function updateDisplay(number){
-   lowerClassTextElement.innerText= number;
+function updateLowerDisplay(){
+   lowerClassTextElement.innerText= values.join('');
+
 }
